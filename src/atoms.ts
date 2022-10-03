2876e51437019f6a10d2a391e99bca7cbb303d83
yeeed711 <rladpwl910@naver.com>
2022-10-03 22:22:36 +0900

    feat: atom 생성
    - toDoState, categoryState, toDoselector 추가

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,6 +1,36 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+
+export enum Categories {
+  '해야할일' = '해야할일',
+  '하는중' = '하는중',
+  '완료됨' = '완료됨',
+}
+export interface IToDo {
+  id: number;
+  text: string;
+  category: Categories;
+}
 
 export const isDarkAtom = atom({
   key: 'isDark',
   default: false,
+});
+
+export const toDoState = atom<IToDo[]>({
+  key: 'toDo',
+  default: JSON.parse(localStorage.getItem('toDos') ?? '[]'),
+});
+
+export const categoryState = atom<Categories>({
+  key: 'category',
+  default: Categories.해야할일,
+});
+
+export const toDoSelector = selector({
+  key: 'toDoSelector',
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
